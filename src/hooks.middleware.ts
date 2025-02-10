a50619fc4eced00d8ef3aca13069f61c1d2f4e22
Simon Holthausen <simon.holthausen@vercel.com>
2025-02-10 12:17:41 +0100

    tmp

--- src/hooks.middleware.ts
+++ src/hooks.middleware.ts
@@ -1,22 +1,15 @@
-import { encrypt, decrypt, reportValue } from '@vercel/flags';
-
 export async function middleware({ cookies, url, reroute }) {
 	if (url.pathname !== '/') return;
 
 	// Retrieve cookies which contain the feature flags.
-	const flags = (await decrypt<Record<string, any>>(cookies.get('flags'))) ?? {};
-
-	// Fall back to random value if this is a new visitor
-	flags.homePageVariant ||= Math.random() > 0.5 ? 'a' : 'b';
-
-	// Report value for integration with Vercels logging etc
-	reportValue('homePageVariant', flags.homePageVariant);
+	// const flags = (await decrypt<Record<string, any>>(cookies.get('flags'))) ?? {};
+	const flag = cookies.get('homePageVariant') || (Math.random() > 0.5 ? 'a' : 'b');
 
 	// Set a cookie to remember the feature flags for this visitor
-	cookies.set('flags', await encrypt(flags), { path: '/' });
+	cookies.set('homePageVariant', flag, { path: '/' });
 
 	return reroute(
 		// Get destination URL based on the feature flag
-		flags.homePageVariant === 'a' ? '/home-a' : '/home-b'
+		flag === 'a' ? '/home-a' : '/home-b'
 	);
 }
