113861946e26b300cd78266f5fdbfa48ba281894
Simon Holthausen <simon.holthausen@vercel.com>
2025-02-20 17:49:16 +0100

    new version

---
+++ cloudflare-middleware.ts
@@ -0,0 +1,34 @@
+import { normalizeUrl } from '@sveltejs/kit';
+
+export async function onRequest({ request, next }) {
+	if (request.url.includes('/middleware')) return new Response('Hello from middleware!');
+
+	const { url, denormalize } = normalizeUrl(request.url);
+
+	console.log('hi there!', url.pathname);
+	if (url.pathname !== '/') return next();
+
+	// Retrieve cookies which contain the feature flags.
+	let flag = split_cookies(request.headers.get('cookie') ?? '')?.['flags'];
+
+	// Fall back to random value if this is a new visitor
+	flag ||= Math.random() > 0.5 ? 'a' : 'b';
+
+	// Get destination URL based on the feature flag
+	request = new Request(denormalize(flag === 'a' ? '/home-a' : '/home-b'), request);
+	// Set a cookie to remember the feature flags for this visitor
+	request.headers.set('Set-Cookie', `flags=${flag}; Path=/`);
+
+	return next(request);
+}
+
+function split_cookies(cookies: string) {
+	return cookies.split(';').reduce(
+		(acc, cookie) => {
+			const [name, value] = cookie.trim().split('=');
+			acc[name] = value;
+			return acc;
+		},
+		{} as Record<string, string>
+	);
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	<nav>
 		<a href="/">to home page</a>
 		<a href="/subpage">to sub page</a>
+		<a href="/middleware">middleware response</a>
 	</nav>
 </header>
 
