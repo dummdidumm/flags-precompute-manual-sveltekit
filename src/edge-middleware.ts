3157507cc0c4eaf8bc9d4084e31143873075f21a
Simon Holthausen <simon.holthausen@vercel.com>
2025-02-22 21:47:08 +0100

    update

---
+++ src/edge-middleware.ts
@@ -0,0 +1,17 @@
+export default async function middleware(request, { next, cookies }) {
+	const url = new URL(request.url);
+
+	if (url.pathname !== '/') return next();
+
+	// Retrieve feature flag from cookies
+	let flag = cookies.get('homePageVariant');
+
+	// Fall back to random value if this is a new visitor
+	flag ||= Math.random() > 0.5 ? 'a' : 'b';
+
+	// Set a cookie to remember the feature flags for this visitor
+	cookies.set('homePageVariant', flag);
+
+	// Get destination URL based on the feature flag
+	return new URL(flag === 'a' ? '/home-a' : '/home-b', url);
+}
